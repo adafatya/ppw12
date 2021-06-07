2577446283f9ebeacc2225f3309cb307837d0ef8
adafatya <aufaditya1895@gmail.com>
2021-06-07 09:16:57 +0700

    fixed

--- baca_berita.php
+++ baca_berita.php
@@ -2,17 +2,21 @@
 require("sambungan.php");
 
 if (!isset($_GET["id"])) {
-	header("Location: index.php");
-	die;
+    header("Location: index.php");
+    die;
 }
 
 if (isset($_POST["komentator"], $_POST["komentar"])) {
-	$kueri = $sambungan->prepare("INSERT INTO komentar 
+    $kueri = $sambungan->prepare("INSERT INTO komentar 
 			(id_berita, komentator, komentar) 
 			VALUES (?, ?, ?);");
-	$kueri->bind_param("iss",
-			$_GET["id"], $_POST["komentator"], $_POST["komentar"]);
-	$hasil = $kueri->execute();
+    $kueri->bind_param(
+        "iss",
+        $_GET["id"],
+        $_POST["komentator"],
+        $_POST["komentar"]
+    );
+    $hasil = $kueri->execute();
 }
 
 $kueri = $sambungan->prepare("SELECT judul, isi, pengirim, tanggal, nama_kategori
@@ -28,17 +32,19 @@ $kueri = "SELECT komentator, komentar, tanggal
 $daftar_komentar = $sambungan->query($kueri);
 
 if ($berita) {
-	$judul = htmlspecialchars($berita["judul"]);
+    $judul = htmlspecialchars($berita["judul"]);
 }
 ?>
 <!DOCTYPE html>
 <html lang="id" dir="ltr">
+
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title><?= $judul ?> | Berita</title>
 	<link rel="stylesheet" href="gaya.css">
 </head>
+
 <body>
 	<nav>
 		<a href="index.php">Halaman Depan</a> |
@@ -46,27 +52,29 @@ if ($berita) {
 		<a href="buat_berita.php">Buat Berita Baru</a>
 	</nav>
 	<hr>
-<?php
+	<?php
 if (!$berita) {
-?>
+    ?>
 	<h1>Berita tidak ditemukan.</h1>
-<?php
+	<?php
 } else {
-	$isi = str_replace("\r\n", "</p>\r\n<p>", htmlspecialchars($berita["isi"]));
-	$pengirim = htmlspecialchars($berita["pengirim"]);
-	$tanggal = trim(strftime('%e %B %G %H.%M.%S', strtotime($berita["tanggal"])));
-	$kategori = htmlspecialchars($berita["nama_kategori"]);
-?>
+        $isi = str_replace("\r\n", "</p>\r\n<p>", htmlspecialchars($berita["isi"]));
+        $pengirim = htmlspecialchars($berita["pengirim"]);
+        $tanggal = trim(strftime('%e %B %G %H.%M.%S', strtotime($berita["tanggal"])));
+        $kategori = htmlspecialchars($berita["nama_kategori"]); ?>
 	<article>
-		<h1><?= $judul ?></h1>
+		<h1><?= $judul ?>
+		</h1>
 		<small>Penulis: <?= $pengirim ?> | Tanggal: <?= $tanggal ?> WIB</small>
-		<p><?= $isi ?></p>
+		<p><?= $isi ?>
+		</p>
 		<small>Kategori: <?= $kategori ?></small>
 	</article>
-<?php
-}
+	<?php
+    }
 ?>
-	<br /> <hr />
+	<br />
+	<hr />
 	<h2>Komentar</h2>
 	<br />
 
@@ -78,22 +86,22 @@ if (!$berita) {
 		<input type="submit" value="kirim">
 	</form>
 
-	<div class="komentar">
-		<?php
-		while ($komentar = $daftar_komentar->fetch_array()) {
-			$komentator = htmlspecialchars($komentar["komentator"]);
-			$komentarnya = str_replace("\r\n", "</p>\r\n<p>", htmlspecialchars($komentar["komentar"]));
-			$tanggal = trim(strftime('%e %B %G %H.%M.%S', strtotime($komentar["tanggal"])));
-		?>
-			<br /> <hr />
-			<h3>
-				<?= $komentator ?>
-			</h3>
-			<small>Tanggal: <?= $tanggal ?> WIB</small><br>
-			<p><?= $komentarnya ?></p>
-		<?php
-		}
-		?>
-	</div>
+	<?php
+        while ($komentar = $daftar_komentar->fetch_array()) {
+            $komentator = htmlspecialchars($komentar["komentator"]);
+            $komentarnya = str_replace("\r\n", "</p>\r\n<p>", htmlspecialchars($komentar["komentar"]));
+            $tanggal = trim(strftime('%e %B %G %H.%M.%S', strtotime($komentar["tanggal"]))); ?>
+	<br />
+	<hr />
+	<h3>
+		<?= $komentator ?>
+	</h3>
+	<small>Tanggal: <?= $tanggal ?> WIB</small><br>
+	<p><?= $komentarnya ?>
+	</p>
+	<?php
+        }
+    ?>
 </body>
+
 </html>
